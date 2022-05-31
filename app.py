0f54d7d47c09b87bd488be6b1f934c58358ee8d9
vlovic <victorlovic@hotmail.com>
2022-05-31 23:32:44 +0100

    Handle POST request from airgradient sensor
    At the moment I just print the request.data to the console and redirect to index.

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'GET':
+        return render_template("index.html")
+
+    elif request.method == 'POST':
+        print(request.data)
+        print("hello")
+        return None
+
+@app.route("/airgradient:dc8c38/measures", methods=['GET', 'POST'])
+def test():
+    if request.method == 'GET':
+        return render_template("index.html")
+
+    elif request.method == 'POST':
+        print(request.data)
+        print("hello")
+        return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(host="192.168.236.192", port=8080, debug=True)
