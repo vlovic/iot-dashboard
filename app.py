1caea05d52a3461cab540125faeed1affd9e17e5
vlovic <victorlovic@hotmail.com>
2022-06-01 16:39:35 +0100

    Clean up app.py

--- app.py
+++ app.py
@@ -2,26 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
-    if request.method == 'GET':
-        return render_template("index.html")
+    return render_template("index.html")
 
-    elif request.method == 'POST':
-        print(request.data)
-        print("hello")
-        return None
-
-@app.route("/airgradient:dc8c38/measures", methods=['GET', 'POST'])
-def test():
-    if request.method == 'GET':
-        return render_template("index.html")
-
-    elif request.method == 'POST':
-        print(request.data)
-        print("hello")
-        return redirect(url_for('index'))
-
-
-if __name__ == '__main__':
-    app.run(host="192.168.236.192", port=8080, debug=True)
+@app.route("/airgradient:dc8c38/measures", methods=['POST'])
+def update_graph():
+    print(request.data)
+    return redirect(url_for('index'))
