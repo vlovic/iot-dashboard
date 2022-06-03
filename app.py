3906c79ff97450d40b5d89b4f8dfac33289b216d
vlovic <victorlovic@hotmail.com>
2022-06-03 10:06:37 +0100

    Stream dummy data to index.html
    Use Server Sent Events to push data from server to client.
    At the moment the data is just fetched from the last row of the db.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, Response, stream_with_context
 from db import get_db, close_connection
 import json
 import datetime
+import time
 
 
 app = Flask(__name__)
@@ -15,16 +16,39 @@ def index():
     return render_template("index.html")
 
 @app.route("/airgradient:dc8c38/measures", methods=['POST'])
-def update_graph():
-    # TODO: add data to sqlite db
+def update_db():
     data_dict = json.loads(request.data.decode("utf-8"))
-    print(data_dict)
+
     db = get_db()
     db.execute(
         "INSERT INTO pm02 (time, pm02) VALUES (?, ?)",
-        (datetime.datetime.now(), data_dict['pm02'])
+        (datetime.datetime.now(), data_dict["pm02"])
     )
     db.commit()
 
     return ""
+
+
+def stream_data():
+    while True:
+        db = get_db()
+
+        data = db.execute("SELECT * FROM pm02 ORDER BY id DESC LIMIT 1;").fetchone()
+        json_data = json.dumps(
+            {
+            "time": data[1],
+            "value": data[2]
+            }
+        )
+
+        yield f"data:{json_data}\n\n"
+        time.sleep(1)
+
+@app.route("/stream")
+def stream():
+    return Response(stream_with_context(stream_data()), mimetype="text/event-stream")
+    
+
+
+
 
--- static/update_graph.js
+++ static/update_graph.js
@@ -1,36 +1,30 @@
 const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
+
+config = {
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: "pm02",
+            data: []
         }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
     }
-});
+}
+
+const myChart = new Chart(ctx, config);
+
+const source = new EventSource("/stream");
+
+source.onmessage = function (event) {
+    console.log("onmessage triggered");
+    const data = JSON.parse(event.data);
+    if (config.data.labels.length === 30) {
+        config.data.labels.shift();
+        config.data.datasets[0].data.shift();
+    }
+    config.data.labels.push(data.time);
+    config.data.datasets[0].data.push(data.value);
+    myChart.update();
+}
+
 
